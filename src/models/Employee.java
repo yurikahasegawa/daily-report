b715dbc5fbe4d19ad64b941480fc0ee38657b513
yurika.hasegawa <yurich0215@gmail.com>
2020-08-17 15:25:07 +0900

    Add Employee index

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -13,10 +13,22 @@ import javax.persistence.Table;
 
 @Table(name = "employees")
 @NamedQueries({
-        @NamedQuery(name = "getAllEmployees", query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"),
-        @NamedQuery(name = "getEmployeesCount", query = "SELECT COUNT(e) FROM Employee AS e"),
-        @NamedQuery(name = "checkRegisteredCode", query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"),
-        @NamedQuery(name = "checkLoginCodeAndPassword", query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass")
+        @NamedQuery(
+                name = "getAllEmployees",
+                query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+                ),
+        @NamedQuery(
+                name = "getEmployeesCount",
+                query = "SELECT COUNT(e) FROM Employee AS e"
+                ),
+        @NamedQuery(
+                name = "checkRegisteredCode",
+                query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
+                ),
+        @NamedQuery(
+                name = "checkLoginCodeAndPassword",
+                query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+                )
 })
 @Entity
 public class Employee {
